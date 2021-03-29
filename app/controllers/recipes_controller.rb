a95b748c8795ff813b8e326a81ccd50be4eb40c6
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-03-29 15:37:12 -0400

    wrote recipe_controller actions

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,43 @@
 class RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new 
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new
+    @recipe.update(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  def show
+    @recipe = Recipe.find_by(id: params[:id])
+  end
+
+  def edit
+    @recipe = Recipe.find_by(id: params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find_by(id: params[:id])
+    @recipe.update(recipe_params)
+    redirect_to recipe_path(@recipe)
+  end
+
+  def delete
+    @recipe = Recipe.find_by(id: params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :link, :thumbnail)
+  end
+
 end
