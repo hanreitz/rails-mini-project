1c164f1cd7c33f6881aa217afb7aee31542d5885
Hannah Lynn Reitzel Rivera <hanreitz@gmail.com>
2021-03-29 14:07:17 -0400

    removed recipe_ingredients controller, added instructions to README

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,20 @@
 class Recipe < ApplicationRecord
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
+
+  def self.get_data
+    response = RestClient.get("http://www.recipepuppy.com/api/")
+    results = JSON.parse(response)["results"]
+
+    results.each do |result|
+      new_recipe = self.create(title: result["title"], link: result["href"], thumbnail: result["thumbnail"])
+      ings = result["ingredients"].split(", ")
+      ings.each do |i|
+        ing = Ingredient.find_or_create_by(name: i)
+        new_recipe.ingredients << ing
+      end
+      new_recipe.save
+    end
+  end
+
 end
